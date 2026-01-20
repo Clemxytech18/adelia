df496ef9226d3752ff694490dc67e8c2327f955d
Clement <clemxytech@gmail.com>
2026-01-20 15:41:34 +0100

    Fix Signup Web recursion crash: Remove animation from overlay text, update build artifacts

--- lib/features/auth/signup_screen.dart
+++ lib/features/auth/signup_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:lottie/lottie.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+
 import '../../core/constants/app_colors.dart';
 import '../shared/underlined_text_field.dart';
 
@@ -386,7 +386,7 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
                                       TextSpan(text: '!'),
                                     ],
                                   ),
-                                ).animate().fadeIn().slideX(),
+                                ),
                               ),
                             ),
                           ),
